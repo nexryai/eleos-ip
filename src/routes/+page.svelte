a7a3fae4f78c9294895a1f955bec85720749d27f
nexryai <61890205+nexryai@users.noreply.github.com>
2025-11-21 06:20:41 +0000

    WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { Input } from "$lib/components/ui/input/index.js";
     import type { PageProps } from './$types';
 
     let { data }: PageProps = $props();
@@ -7,7 +8,11 @@
 </script>
 
 <div class="mt-24 mx-8">
-    <h1 class="text-3xl font-bold mb-4">Your IP Information</h1>
+    <div class="flex justify-center">
+        <h1 class="text-2xl font-bold mb-4">Find out more information about your IP address!</h1>
+    </div>
+    
+    <Input defaultValue={ipInfo.ip} />
     {#if ipInfo}
         <ul class="list-disc list-inside">
             <li><strong>IP:</strong> {ipInfo.ip}</li>
