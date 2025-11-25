81af9287bd9ffcf06e991de57abfef4476a63399
nexryai <61890205+nexryai@users.noreply.github.com>
2025-11-25 09:12:07 +0000

    Fix footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,16 +15,16 @@
         {@render children()}
     </main>
 
-    <footer class="mt-32 h-24 px-6 flex justify-between items-center w-full">
+    <footer class="mt-32 h-24 px-6 flex md:flex-row flex-col justify-between items-center w-full">
         <div>
             <p class="text-sm text-muted-foreground">
                 &copy; {new Date().getFullYear()} IP Info Finder. All rights reserved.
             </p>
             <p class="text-sm text-muted-foreground">
-                Map data: &copy;mapbox & OpenStreetMap. 
+                Map data: &copy;Mapbox & &copy;OpenStreetMap
             </p>
         </div>
-        <div>
+        <div class="md:mb-0 mb-24">
             <p class="text-sm text-muted-foreground">
                 Powered by IPinfo & IPLocate.io.
             </p>
