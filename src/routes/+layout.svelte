57a9e04a441bd5cafe08c1dc1cea4a199f12a271
nexryai <61890205+nexryai@users.noreply.github.com>
2025-11-25 08:20:56 +0000

    Footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,25 @@
     <link rel="icon" href={favicon}>
 </svelte:head>
 
-{@render children()}
+<div class="min-h-screen flex flex-col">
+    
+    <main class="flex-1">
+        {@render children()}
+    </main>
+
+    <footer class="mt-32 h-24 px-6 flex justify-between items-center w-full">
+        <div>
+            <p class="text-sm text-muted-foreground">
+                &copy; {new Date().getFullYear()} IP Info Finder. All rights reserved.
+            </p>
+            <p class="text-sm text-muted-foreground">
+                Map data: &copy;mapbox & OpenStreetMap. 
+            </p>
+        </div>
+        <div>
+            <p class="text-sm text-muted-foreground">
+                Powered by IPinfo & IPLocate.io.
+            </p>
+        </div>
+    </footer>
+</div>
