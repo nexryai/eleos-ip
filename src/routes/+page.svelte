4ebb801e9c8492acc7f69fcc794a02d30291f9d5
nexryai <61890205+nexryai@users.noreply.github.com>
2025-11-25 08:02:55 +0000

    WIP: Top page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
     import { Badge } from "$lib/components/ui/badge/index.js";
     import { Button } from "$lib/components/ui/button";
     import { Input } from "$lib/components/ui/input/index.js";
-    
     import type { PageProps } from "./$types";
 
     let { data }: PageProps = $props();
@@ -28,23 +27,36 @@
 </script>
 
 <div class="mt-24 mx-8">
-    <div class="flex justify-center">
-        <h1 class="text-2xl font-bold mb-4">Find out more information about your IP address!</h1>
+    <div class="flex justify-between items-center">
+        <div>
+            <h1 class="text-2xl font-bold mb-4">
+                Find out more information <br>
+                about IP address!
+            </h1>
+        </div>
+        <div class="flex justify-start">
+            <img src="/undraw_global-team_8jok.svg" alt="Global Team Illustration" class="w-64 ml-6">
+        </div>
+    </div>
+    <div class="flex justify-start w-full">
+        <div class="relative">
+            <div class="-z-50 absolute bottom-1/3 left-0 right-0 top-0 my-auto w-96 h-32 rotate-45 bg-linear-to-r from-yellow-400 to-yellow-300 blur-3xl opacity-20 dark:opacity-30"></div>
+        </div>
     </div>
 
-    <div class="flex gap-4">
+    <div class="mt-10">
+        {#if ipInfo.asn}
+            <div class="flex gap-2 mt-3">
+                <Badge variant="outline">{ipInfo.country}</Badge>
+                <Badge variant="outline">{ipInfo.asn}</Badge>
+                <Badge variant="outline">{ipInfo.as_domain}</Badge>
+            </div>
+        {/if}
+    </div>
+
+    <div class="mt-3 flex gap-4 max-w-lg">
         <Input bind:value={inputValue} onkeydown={handleKeyDown} placeholder="IP address..."/>
 
         <Button disabled={!allowSearch} onclick={handleSearch}>Search</Button>
     </div>
-
-    {#if ipInfo}
-        <div class="flex gap-2 mt-3">
-            <Badge variant="outline">{ipInfo.country}</Badge>
-            <Badge variant="outline">{ipInfo.asn}</Badge>
-            <Badge variant="outline">{ipInfo.as_domain}</Badge>
-        </div>
-    {:else}
-        <p>Unable to retrieve IP information.</p>
-    {/if}
 </div>
